768a984fcbb7f7f90da52683ff4df7e1103d4d49
Norbert_Varga <109357322+Madlion0187@users.noreply.github.com>
2025-05-27 16:55:31 +0200

    first commit

---
+++ php_01.php
@@ -0,0 +1,36 @@
+<?php
+
+$name;
+
+$age = 18;
+
+$name = "Norbi";
+
+$int_number = 25;
+$float_number = 45.55;
+
+$string_type = "Learn php";
+
+$boolean_type = true;
+
+$array_type1 = ["Jack", "Mike", "Nadja"];
+$mixed_array = array(15,25,"560",true,false);
+
+//$nothing;
+$nothing1 = null;
+
+//echo $name . "\n";
+//echo $age . PHP_EOL;
+//echo $int_number . PHP_EOL;
+//echo $float_number . PHP_EOL;
+//echo $boolean_type . PHP_EOL;
+
+//var_dump($array_type1);
+//print_r($mixed_array);
+//var_dump($nothing);
+//var_dump($nothing1);
+
+$number = 9;
+$city = "Beijing";
+
+printf("\nThere are %d million bicycles in %s \n\n", $number, $city);
